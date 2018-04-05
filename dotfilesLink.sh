8f549f0cc3112562422fa3c09ca6bea6cdc4640c
masumi <taketo224@gmail.com>
2018-04-06 00:08:24 +0900

    add symbolic link

---
+++ dotfilesLink.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ln -sf ~/dotfiles/compVim ~/.config/nvim
+ln -sf ~/dotfiles/dein ~/.config/nvim
+ln -sf ~/dotfiles/mySnippets ~/.config/nvim
+ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/lazy.toml ~/.config/nvim/lazy.toml
+ln -sf ~/dotfiles/plugins.toml ~/.config/nvim/plugins.toml
