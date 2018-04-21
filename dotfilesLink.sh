2be4d314ec40d307ab2e2300534adf7648c74f68
Your Name <you@example.com>
2018-04-21 19:48:35 +0900

    deleted various files

--- dotfilesLink.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ln -sf $HOME/dotfiles/compVim $HOME/.config/nvim
-ln -sf $HOME/dotfiles/mySnippets $HOME/.config/nvim
-ln -sf $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
---
+++ link.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ln -sf $HOME/dotfiles/init.vim $XDG_CONFIG_HOME/nvim/init.vim
