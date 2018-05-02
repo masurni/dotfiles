24ab0e49892d676626646eb3105c80aac13a4f4a
masumi <taketo224@gmail.com>
2018-05-02 11:16:20 +0900

    va

--- link.sh
+++ link.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-ln -sf $HOME/dotfiles/init.vim $XDG_CONFIG_HOME/nvim/init.vim
+if ! grep XDG_CONFIG_HOME $HOME/.bashrc > /dev/null; then
+	echo "export XDG_CONFIG_HOME=$HOME/.config" >> $HOME/.bashrc
+	. $HOME/.bashrc
+fi
+ln -sf ${PWD}/init.vim $XDG_CONFIG_HOME/nvim/init.vim
